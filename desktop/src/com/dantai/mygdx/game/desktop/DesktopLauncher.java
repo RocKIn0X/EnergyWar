646ec741e80d213f5b27df830333bbacd9518237
Dantai Limlawan <dantai39@gmail.com>
2016-10-17 19:28:49 +0700

    create project

---
+++ desktop/src/com/dantai/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dantai.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dantai.mygdx.game.EnergyWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EnergyWar(), config);
+	}
+}
