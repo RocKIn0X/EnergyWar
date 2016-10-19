57cc80889c0c38d0afa4f00031f1d74267d69238
Dantai Limlawan <dantai39@gmail.com>
2016-10-19 08:17:46 +0700

    add GameScreen

--- desktop/src/com/dantai/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dantai/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
                 config.title = "Energy war";
-                config.width = 1200;
+                config.width = 1280;
                 config.height = 800;
 		new LwjglApplication(new EnergyWar(), config);
 	}
