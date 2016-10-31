062d268b4fef937e1dd3da4f7f8fcb72e74a0a21
Dantai Limlawan <dantai39@gmail.com>
2016-10-31 10:54:57 +0700

    Robot movement by input

--- core/src/com/dantai/mygdx/game/GameScreen.java
+++ core/src/com/dantai/mygdx/game/GameScreen.java
@@ -6,6 +6,7 @@
 package com.dantai.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -34,10 +35,27 @@ public class GameScreen extends ScreenAdapter{
     }
     
     @Override
-    public void render(float delta){       
+    public void render(float delta){     
+        update(delta);
+        
         Gdx.gl.glClearColor(0, 0, 0.5f, 1);
 	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         worldRenderer.render(delta);
+    }
+    
+    private void update(float delta){
+        if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            robot.move(robot.DIRECTION_UP);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            robot.move(robot.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            robot.move(robot.DIRECTION_DOWN);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            robot.move(robot.DIRECTION_LEFT);
+        }
     }
 }
--- core/src/com/dantai/mygdx/game/Robot.java
+++ core/src/com/dantai/mygdx/game/Robot.java
@@ -14,8 +14,28 @@ import com.badlogic.gdx.math.Vector2;
 public class Robot {
     private Vector2 position;
     
+    public static final int DIRECTION_UP = 1;
+    public static final int DIRECTION_RIGHT = 2;
+    public static final int DIRECTION_DOWN = 3;
+    public static final int DIRECTION_LEFT = 4;
+    public static final int DIRECTION_STILL = 0;
+    public static final int SPEED = 5;
+    
+    private static final int [][] DIR_DIFF = new int[][]{
+        {0,0},
+        {0,1},
+        {1,0},
+        {0,-1},
+        {-1,0}
+    };
+    
     public Robot(int x, int y){
         position = new Vector2(x, y);
+    }
+    
+    public void move(int dir){
+        position.x += SPEED * DIR_DIFF[dir][0];
+        position.y += SPEED * DIR_DIFF[dir][1];
     }
     
     public Vector2 getPosition(){
