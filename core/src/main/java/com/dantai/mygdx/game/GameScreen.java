d1cdc9c0fcabebe95e5a33072fba177d95c3339b
Dantai Limlawan <dantai39@gmail.com>
2016-11-07 11:45:53 +0700

    Add gameCam on GameScreen

--- core/src/main/java/com/dantai/mygdx/game/GameScreen.java
+++ core/src/main/java/com/dantai/mygdx/game/GameScreen.java
@@ -9,7 +9,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  *
@@ -19,18 +23,30 @@ public class GameScreen extends ScreenAdapter {
 
     private EnergyWar game;
     private Texture robotTexture;
+    private Texture background;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public GameScreen(EnergyWar game) {
         this.game = game;
         robotTexture = new Texture("robot.png");
+        background = new Texture("Background.png");
+        gameCam = new OrthographicCamera(background.getWidth(), background.getHeight());
+        gameCam.translate(background.getWidth() / 2, background.getHeight() / 2, 0);
+        gameCam.update();
+        gamePort = new FitViewport(800, 480, gameCam);
     }
 
     @Override
     public void render(float deltaTime) {
+        SpriteBatch batch = game.batch;
+        
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(robotTexture, 100, 100);
-        game.batch.end();
+        batch.setProjectionMatrix(gameCam.combined);
+        batch.begin();
+        batch.draw(background, 0, 0);
+        batch.draw(robotTexture, 100, 100);
+        batch.end();
     }
 }
--- desktop/src/com/dantai/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dantai/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
    	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Energy war";
-        config.width = 1280;
-        config.height = 800;
+        config.width = 800;
+        config.height = 480;
         new LwjglApplication(new EnergyWar(), config);
     }
 }
